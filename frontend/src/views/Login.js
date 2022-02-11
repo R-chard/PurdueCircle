df4f44221ae6fe5f479c4c78ae620f928866ba25
johanan-daniel <70776118+johanan-daniel@users.noreply.github.com>
2022-02-10 19:56:00 -0500

    css stuff

--- frontend/src/views/Login.js
+++ frontend/src/views/Login.js
@@ -5,24 +5,22 @@ const Login = (props) => {
     const { submit, username, usernameHandler, password, passwordHandler } = props.inputFunctions
 
     return (
-        <div className="loginSection container">
-            <div className="row">
-                <div className="loginModal">
-                    <h1>Purdue Circle</h1>
-                    <div className="inputContainer">
-                        <form onSubmit={submit}>
-                            <div className="field">
-                                Username: <input value={username} onChange={usernameHandler} />
-                            </div>
-                            <div className="field">
-                                Password: <input type={"password"} value={password} onChange={passwordHandler} />
-                            </div>
-                            <div>
-                                <button type="submit">login</button>
-                            </div>
-                        </form>
+        <div className="loginModal">
+            <h1>Purdue Circle</h1>
+            <div className="inputContainer">
+                <form onSubmit={submit}>
+                    <h3>Login</h3>
+                    <div className="field">
+                        <input value={username} onChange={usernameHandler} placeholder={'username'}/>
                     </div>
-                </div>
+                    <div className="field">
+                        <input type={"password"} value={password} onChange={passwordHandler} placeholder={'password'}/>
+                    </div>
+                    <div>
+                        <input type={"submit"} value={"login"}/>
+                        {/* <button type="submit">login</button> */}
+                    </div>
+                </form>
             </div>
         </div>
     )

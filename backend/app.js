7b19fa6ae35910e6eb5bade8ae882a5a699698a6
richard <richardding1999@gmail.com>
2022-02-12 01:45:57 -0500

    Added useful dependecies + added `npm run both` + linked database

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const cors = require("cors")
+const bodyParser = require("body-parser")
 const app = express()
 const port = 5000
+
+app.use(bodyParser.json())
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -9,3 +15,15 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+mongoose.connect(
+  "mongodb+srv://admin:P4rdue@cluster0.g5hjd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", 
+  {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+  },
+  () => {
+    var status = mongoose.connection.readyState == 1 ? "connected" : "disconnected"
+    console.log("Database is " + status)
+  }
+);

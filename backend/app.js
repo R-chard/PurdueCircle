6e679afc7bc942dcd6718ad4a2d3e1261dd64405
richard <richardding1999@gmail.com>
2022-02-13 11:55:14 -0500

    Added button for basic signup

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose')
 const cors = require("cors")
 const bodyParser = require("body-parser")
 const app = express()
+const userRoutes = require("./routes/user-routes")
 const port = 5000
-
+ 
 app.use(bodyParser.json())
 app.use(cors())
 
@@ -12,10 +13,15 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+// PLACE ANY ENDPOINT YOU WANT TO DIRECT BELOW
+app.use("/api/user",userRoutes)
+
+// Indicate to the developer that the server is working. Doesn't really do much
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
+// Establish connection with database
 mongoose.connect(
   "mongodb+srv://admin:P4rdue@cluster0.g5hjd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", 
   {

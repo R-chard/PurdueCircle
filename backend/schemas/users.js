3f8034d080a0a691c2f1327393ed3641eeffac2b
richard <richardding1999@gmail.com>
2022-02-19 16:49:46 -0500

    Minor changes to user schema

--- backend/schemas/users.js
+++ backend/schemas/users.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose")
 
-// Defining the fields each User has
 const userSchema = new mongoose.Schema({
-    username: {type:String,required: true},
-    email: {type: String, required:true},
-    password: {type:String, required: true},
-    profile_img: {type:String, required:true},
+    username: {type:String,required:true,unique:true},
+    email: {type:String, required:true,unique:true},
+    password: {type:String, required:true},
+    profile_img: String,
+    topics_followed: [{type:mongoose.Types.ObjectId, ref:"Topic"}],
+    biography:String,
+    users_followed: [{type:mongoose.Types.ObjectId, ref:"User"}]
 })
 
 // Tell mongoDB this is a database schema

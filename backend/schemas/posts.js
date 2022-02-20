e7f102daebdf9083dd55de493c73877bc9aa2d69
richard <richardding1999@gmail.com>
2022-02-19 19:26:18 -0500

    added post schema

---
+++ backend/schemas/posts.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose")
+
+// Defining the fields each User has
+const postSchema = new mongoose.Schema({
+    author: {type:mongoose.Types.ObjectId, ref:"User",required:true},
+    datePosted:{type:Date, required:true},
+    message: {String, required:true, maxlength:500},
+    postedAnon: {Boolean, required:true},
+    topics:[{type:mongoose.Types.ObjectId, ref:"Topic"}],
+    comments:[{
+        author:{type:mongoose.Types.ObjectId, ref:"User"},
+        datePosted: Date,
+        message: String
+    }],
+    likes: Number
+})
+
+// Tell mongoDB this is a database schema
+module.exports = mongoose.model("Post",postSchema);
--- backend/schemas/users.js
+++ backend/schemas/users.js
@@ -1,13 +1,14 @@
 const mongoose = require("mongoose")
 
 const userSchema = new mongoose.Schema({
-    username: {type:String,required:true,unique:true},
+    username: {type:String,required:true,unique:true,maxlength:32},
     email: {type:String, required:true,unique:true},
     password: {type:String, required:true},
     profile_img: String,
     topics_followed: [{type:mongoose.Types.ObjectId, ref:"Topic"}],
     biography:String,
-    users_followed: [{type:mongoose.Types.ObjectId, ref:"User"}]
+    users_followed: [{type:mongoose.Types.ObjectId, ref:"User"}],
+    saved_posts:[{type:mongoose.Types.ObjectId, ref:"Post"}]
 })
 
 // Tell mongoDB this is a database schema

64f8fd1d849f60e9541f203680b59e34619b8de6
Manny6902 <mpylla@purdue.edu>
2022-03-29 16:58:46 -0400

    Update Profile.js

--- frontend/src/views/Profile.js
+++ frontend/src/views/Profile.js
@@ -4,22 +4,17 @@ import "../styles/Profile.css"
 import axios from "axios"
 import Button from "../components/Button"
 import Tab from "../components/Tab"
+import InlinePost from "../components/InlinePost"
+import '../styles/Home.css'
+import '../styles/PostView.css'
 
 const Profile = (props) => {
     
     const [data, setData] = useState(null)
+    const [tabContent, setTabContent] = useState([])
     const [followed, setFollowed] = useState(null);
     const [followedHandler, setFollowedHandler] = useState(null);
-    const tabContent = [
-        {
-            title: "Posts",
-            content: `Posts go here`
-        },
-        {
-            title: "Interactions",
-            content: `Interactions go here`
-        }
-    ];
+    
     const location = useLocation()
     useEffect(()=>{
         axios.get("/api/user" + location.pathname,{
@@ -31,7 +26,43 @@ const Profile = (props) => {
             setFollowed("Follow")
             //setFollowedHandler(followHandler)
         })
+
+        //replace with api
+        const comment1 = { author: "Jeff", date: new Date("2022-03-03T22:11:12.129+00:00"), id: 421432, message: "My name is Jeff"}
+        const comment2 = { author: "A person", date: new Date("2022-02-30T22:11:12.129+00:00"), id: 91248057, message: "Beep boop"}
+        const comment3 = { author: "Tim Apple", date: new Date("2022-01-13T22:11:12.129+00:00"), id: 6453141, message: "Good Morning!"}
+        const comment4 = { author: "Moana", date: new Date("2022-01-31T22:11:12.129+00:00"), id: 9184537, message: "The ocean is calling me and I must go"}
+        const comment5 = { author: "Joe", date: new Date("2022-02-17T22:11:12.129+00:00"), id: 3978514, message: "Who am I? What is existence but the affirmation by others that you exist? What is love?"}
+        const comment6 = { author: "Jimothy", date: new Date("2022-03-23T22:11:12.129+00:00"), id: 340789541, message: "Comment sections amirite"}
+
+        const objCommentsArray1 = [comment1, comment2, comment5]
+        const objCommentsArray2 = [comment3, comment4, comment5, comment6]
+
+        const objProfilePic = "https://imageio.forbes.com/specials-images/imageserve/5fe74d45a9c2a2d204db2948/0x0.jpg?format=jpg&width=1200&fit=bounds";
+        const post1 = { _id: 23451234312, author: { username: "jimothy", profile_img: objProfilePic }, postedAnon: false, message: `This is a post with some text that shows how a post with some text can be
+        displayed`, likes: 4, datePosted: new Date("2022-02-24T22:11:12.129+00:00"), 
+            comments: objCommentsArray1, topics: ["posts", "society"], isLiked: false }
+
+        const post2 = { _id: 8973241613, author: { username: "dave", profile_img: objProfilePic }, postedAnon: false, message: `This is a very long post to show how
+        a long post would be displayed. This message has been brought to you by Raid Shadow Legeneds. Raid: Shadow Legends is a mobile-fantasy 
+        RPG game for mobile and PC developed by Plarium Games. The game takes place in Teleria, which has been subjugated by the Dark Lord 
+        Siroth. The player goes through twelve levels in single player mode, with a multiplayer PVP mode deciding player rankings`, 
+        likes: 10, datePosted: new Date("2022-03-05T22:11:12.129+00:00"), 
+            comments: objCommentsArray2, topics: ["posts", "society"], isLiked: false }
+        
+        setTabContent([
+            {
+                title: "Posts",
+                content: [post1, post2, post2, post2]
+            },
+            {
+                title: "Interactions",
+                content: []
+            }
+        ])
+    //replace with api
     },[location.pathname])
+
 
     const followHandler =() => {
         axios.post("/api/user/follow",{
@@ -61,7 +92,7 @@ const Profile = (props) => {
 
     return (
         <div>
-            {data && (<div className={'contents profile'}>
+            {data && tabContent && (<div className={'contents profile'}>
         <h1>Profile</h1>
         <div style={{maxWidth:"800px",margin:"0px auto"}}>
             <div style={{
@@ -95,11 +126,17 @@ const Profile = (props) => {
 
             {/* Userline tabs */}
             
-            <div className="col text-center">
+            <div>
                 <Tab>
                 {tabContent.map((tab, idx) => (
                     <Tab.TabPane key={`Tab-${idx}`} tab={tab.title}>
-                    {tab.content}
+                    {<div className="container postView">
+                        {tab.content.length === 0 ? <div>There are no posts to show </div> : (
+                            tab.content.map(post => (
+                                <InlinePost key={post._id} post={post}/>
+                            )))}
+                        </div>
+                    }
                     </Tab.TabPane>
                 ))}
                 </Tab>

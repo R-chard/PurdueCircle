c948ae8cc7fd71e8d3b337ca7362dae2fc292db5
Manny6902 <mpylla@purdue.edu>
2022-03-29 16:02:24 -0400

    Added tab ui

--- frontend/src/views/Profile.js
+++ frontend/src/views/Profile.js
@@ -3,12 +3,23 @@ import { Link,useLocation } from "react-router-dom"
 import "../styles/Profile.css"
 import axios from "axios"
 import Button from "../components/Button"
+import Tab from "../components/Tab"
 
 const Profile = (props) => {
     
     const [data, setData] = useState(null)
     const [followed, setFollowed] = useState(null);
     const [followedHandler, setFollowedHandler] = useState(null);
+    const tabContent = [
+        {
+            title: "Posts",
+            content: `Posts go here`
+        },
+        {
+            title: "Interactions",
+            content: `Interactions go here`
+        }
+    ];
     const location = useLocation()
     useEffect(()=>{
         axios.get("/api/user" + location.pathname,{
@@ -66,34 +77,36 @@ const Profile = (props) => {
                 </div>
                 {/* Right Side Info */}
                 <div>
-                    <div style={{display:"flex", justifyContent:"space-around", margin:"0px 0px", height:"20%"}}>
+                    <div style={{display:"flex", justifyContent:"space-around", margin:"5px 0px", height:"20%"}}>
                         <h1>{data.username}</h1>
                         <Button className="formSubmit" text={followed} onClick={followedHandler}></Button>
                     </div>         
-                    <div style={{display:"flex",justifyContent:"space-between", width:"120%"}}>
+                    <div style={{display:"flex",justifyContent:"space-between", margin: "15px 0px", width:"120%"}}>
                         <h6>{data.posts.length} posts</h6>
                         <Link to={'/followers/' +data.username} className="link">{data.users_followed.length} followers</Link>
                         <Link to={'/following/' +data.username} className="link">{data.users_following.length} following</Link>
                         <Link to={'/topics/' +data.username} className="link">{data.topics_followed.length} topics</Link>
                     </div>
-                    <h2>{data.name}</h2>
-                    <h4>{data.biography}</h4>
+                    <h6>{data.name}</h6>
+                    <h6>{data.biography}</h6>
                 </div>
             </div>
 
-            <div className="timeline">
-                <img className="post" src="https://static1.makeuseofimages.com/wordpress/wp-content/uploads/2012/06/wallpaper.png"/>
-                <img className="post" src="https://static1.makeuseofimages.com/wordpress/wp-content/uploads/2012/06/wallpaper.png"/>
-                <img className="post" src="https://static1.makeuseofimages.com/wordpress/wp-content/uploads/2012/06/wallpaper.png"/>
-                <img className="post" src="https://static1.makeuseofimages.com/wordpress/wp-content/uploads/2012/06/wallpaper.png"/>
-                <img className="post" src="https://static1.makeuseofimages.com/wordpress/wp-content/uploads/2012/06/wallpaper.png"/>
-                <img className="post" src="https://static1.makeuseofimages.com/wordpress/wp-content/uploads/2012/06/wallpaper.png"/>
+            {/* Userline tabs */}
+            
+            <div className="col text-center">
+                <Tab>
+                {tabContent.map((tab, idx) => (
+                    <Tab.TabPane key={`Tab-${idx}`} tab={tab.title}>
+                    {tab.content}
+                    </Tab.TabPane>
+                ))}
+                </Tab>
             </div>
+            
         </div>
     </div>)}
         </div>
-        
-        
     ) 
 }
 
